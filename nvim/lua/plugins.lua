a56c1ddbe0357cdaf59760390e304da831a64c5a
Chris Johnson <cjohnson@twilio.com>
2025-05-14 19:56:59 +0100

    AI magic

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,6 +1,11 @@
+local opts = require('opts')
+
 return {
   'numToStr/Comment.nvim',
   'nmac427/guess-indent.nvim',
+  'mason-org/mason-lspconfig.nvim',
+  'neovim/nvim-lspconfig',
+  'nvim-tree/nvim-web-devicons',
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
@@ -19,23 +24,6 @@ return {
     'mason-org/mason.nvim',
     lazy = false,
     opts = {},
-  },
-  'mason-org/mason-lspconfig.nvim',
-  'neovim/nvim-lspconfig',
-  'nvim-tree/nvim-web-devicons',
-  {
-    'saghen/blink.cmp',
-    version = '1.*',
-    opts = {
-      completion = {
-        menu = {
-          auto_show = true,
-        },
-      },
-      keymap = {
-        ['<Tab>'] = { 'select_and_accept' },
-      },
-    },
   },
   {
     'nvim-telescope/telescope.nvim',
@@ -87,14 +75,78 @@ return {
     },
   },
   {
-    "nvim-tree/nvim-tree.lua",
+    'nvim-tree/nvim-tree.lua',
     lazy = false,
     dependencies = {
-      "nvim-tree/nvim-web-devicons",
+      'nvim-tree/nvim-web-devicons',
     },
     opts = {
       view = {
         width = 60,
+      },
+    },
+  },
+  {
+    'nomnivore/ollama.nvim',
+    enabled = opts.llm == 'ollama',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      model = 'qwen2.5-coder',
+      serve = {
+        on_start = true,
+      },
+    },
+    keys = {
+      {
+        '<leader>oo',
+        ':<c-u>lua require("ollama").prompt()<cr>',
+        mode = { 'n', 'v' },
+      },
+      {
+        '<leader>og',
+        ':<c-u>lua require("ollama").prompt("Generate_Code")<cr>',
+        mode = { 'n', 'v' },
+      },
+      {
+        '<leader>om',
+        ':<c-u>lua require("ollama").prompt("Modify_Code")<cr>',
+        mode = { 'n', 'v' },
+      },
+    },
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    enabled = opts.llm == 'copilot',
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+    },
+  },
+  {
+    'saghen/blink.cmp',
+    version = '1.*',
+    dependencies = {
+      'fang2hou/blink-copilot',
+    },
+    opts = {
+      sources = {
+        default = { 'lsp', 'path', 'copilot', 'buffer' },
+        providers = {
+          copilot = {
+            name = 'copilot',
+            module = 'blink-copilot',
+            score_offset = 100,
+            async = true,
+          },
+        },
+      },
+      completion = {
+        menu = {
+          auto_show = true,
+        },
+      },
+      keymap = {
+        ['<CR>'] = { 'accept', 'fallback' },
       },
     },
   },
