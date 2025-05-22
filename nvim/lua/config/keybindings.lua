4c74b9fbb51211d6f67be4593261558ee4bc5963
Chris Johnson <cjohnson@twilio.com>
2025-05-22 14:52:31 +0100

    feat(lsp): add nvim-lightbulb and code action keymap
    - Added 'kosayoda/nvim-lightbulb' plugin with autocmd enabled to show
      available code actions.
    - Introduced <leader>a keybinding in normal and visual modes to trigger
      LSP code actions via `vim.lsp.buf.code_action`.
    - Minor formatting adjustments to existing keymap definitions for
      consistency.

--- nvim/lua/config/keybindings.lua
+++ nvim/lua/config/keybindings.lua
@@ -1,4 +1,5 @@
 -- non-plugin-specific keybindings
 
-vim.keymap.set({ 'n', 'v'}, '<leader>q', '<cmd>qa<cr>', { desc = 'quit' })
-vim.keymap.set({ 'n', 'v'}, '<leader>w', '<cmd>w<cr>', { desc = 'write' })
+vim.keymap.set({ 'n', 'v' }, '<leader>q', '<cmd>qa<cr>', { desc = 'quit' })
+vim.keymap.set({ 'n', 'v' }, '<leader>w', '<cmd>w<cr>', { desc = 'write' })
+vim.keymap.set({ 'n', 'v' }, '<leader>a', vim.lsp.buf.code_action, { desc = 'Show code actions' })
