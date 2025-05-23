9e7085191fd88450e81c0034c6e116c54fad3608
Chris Johnson <cjohnson@twilio.com>
2025-05-23 11:42:25 +0100

    fix(copilot): close CopilotChat after copying message
    Refactor commit message copy logic to close the CopilotChat window
    after copying the answer to the clipboard. This streamlines the user
    workflow by automatically dismissing the chat once the message is
    copied.

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,14 +1,14 @@
 local opts = require('opts')
 
 local function copy_commit_message()
-  local chat = require('CopilotChat').chat
+  local chatParent = require('CopilotChat')
+  local chat = chatParent.chat
 
   if chat:get_closest_section().answer then
     local t = vim.loop.new_timer()
     t:start(500, 0, vim.schedule_wrap(copy_commit_message))
   else
     local answer = chat:get_closest_section('answer').content
-    -- remove the first and last line of answer and assign it to a variable called turtle
     local msg = ''
     for line in answer:gmatch('([^\n]*)\n?') do
       if not line:match('^```') then
@@ -17,6 +17,7 @@ local function copy_commit_message()
     end
     vim.fn.setreg('*', msg)
     vim.notify('Copied commit message to clipboard')
+    chatParent.close()
   end
 end
 
